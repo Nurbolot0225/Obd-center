d244dcb24f501d3406c6af828fcfa73bcc2ca623
Nurbolot0225 <nurchik0225@gmail.com>
2022-04-23 22:38:22 +0600

    update file

--- src/js/script.js
+++ src/js/script.js
@@ -28,9 +28,23 @@ let faqs__item = function () {
 }
 faqs__item();
 
+$(document).ready(function(){
+	$('.menu a').click(function(e){
+    	if($(this).attr('href').indexOf('#') != -1){ // Проверяем, является и ссылка действительно якорной ссылкой.
+			e.preventDefault(); // Отменяем событие перехода.
+            var href = $(this).attr('href').replace('#', ''); // Получаем из якорной ссылки нужный ID элемента, к которому будет происходить переход.
+			
+			if($('#'+href).length > 0){ // Проверяем, существует ли на странице нужный нам элемент.
+            	var tophref = $('body').find('#'+href).offset().top; // Получаем координаты элемента, относительно начала страницы.
+            	$('html, body').animate({scrollTop: tophref}, 800); // Создаём анимацию скрола к нужному элементу.
+			}
+		}
+    });
+});
+
 // masketinput
 
-$('input[name=phone]').mask("0 (999) 999-99-99");
+$('input[name=phone]').mask("+996 (111) 11-11-11");
 
 // Smooth scroll and pageup 
 

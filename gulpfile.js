60a1b18d0a6709e9f025c1ef4868f7eaa1b14521
WayupKG <Adikgk232323@gmail.com>
2022-04-24 12:48:25 +0600

    gulp include html has been tested

--- gulpfile.js
+++ gulpfile.js
@@ -6,24 +6,22 @@ const autoprefixer = require('gulp-autoprefixer');
 const rename = require("gulp-rename");
 
 gulp.task('server', function() {
-
     browserSync({
         server: {
             baseDir: "src"
         }
     });
-
     gulp.watch("src/*.html").on('change', browserSync.reload);
 });
 
 gulp.task('styles', function() {
     return gulp.src("src/sass/**/*.+(scss|sass)")
-        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-        .pipe(rename({suffix: '.min', prefix: ''}))
-        .pipe(autoprefixer())
-        .pipe(cleanCSS({compatibility: 'ie8'}))
-        .pipe(gulp.dest("src/css"))
-        .pipe(browserSync.stream());
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(rename({suffix: '.min', prefix: ''}))
+    .pipe(autoprefixer())
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(gulp.dest("src/css"))
+    .pipe(browserSync.stream());
 });
 
 gulp.task('watch', function() {

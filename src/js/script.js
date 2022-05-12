22a37194f90a703e11fefccbc0be4d194fc376ff
Nurbolot0225 <nurchik0225@gmail.com>
2022-05-12 19:34:48 +0600

    Updeted js file scrollTop

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
 	$('.header a').click(function(e){
-    	if($(this).attr('href').indexOf('#') != -1){ // Проверяем, является и ссылка действительно якорной ссылкой.
-			e.preventDefault(); // Отменяем событие перехода.
-            var href = $(this).attr('href').replace('#', ''); // Получаем из якорной ссылки нужный ID элемента, к которому будет происходить переход.
+    	if($(this).attr('href').indexOf('#') != -1){ 
+			e.preventDefault(); 
+            var href = $(this).attr('href').replace('#', ''); 
 			
-			if($('#'+href).length > 0){ // Проверяем, существует ли на странице нужный нам элемент.
-            	var tophref = $('body').find('#'+href).offset().top; // Получаем координаты элемента, относительно начала страницы.
-            	$('html, body').animate({scrollTop: tophref}, 800); // Создаём анимацию скрола к нужному элементу.
+			if($('#'+href).length > 0){ 
+            	var tophref = $('body').find('#'+href).offset().top; 
+            	$('html, body').animate({scrollTop: tophref}, 800); 
 			}
 		}
     });
